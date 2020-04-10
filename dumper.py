28bca153c2d243a10399884bd3b952dd1b9fc668
aureon <jacopo.sinigaglia@gmail.com>
2020-04-10 19:14:14 +0200

    dumper dumps only on realm grinder focused

--- dumper.py
+++ dumper.py
@@ -1,11 +1,11 @@
 from pynput.mouse import Listener
-
+from auto import wfocused
 
 def on_move(x, y):
     pass
 
 def on_click(x, y, button, pressed):
-    if pressed:
+    if pressed and wfocused():
         print("{}, {}".format(x, y))
 
 def on_scroll(x, y, dx, dy):
